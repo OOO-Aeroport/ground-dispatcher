3c6134b45d0c96de4ddbcbeeea554028d9e8176e
Deathkiller2009 <popovartem439@gmail.com>
2025-03-11 13:56:04 +0300

    Initial commit

---
+++ src/main/java/ru/deathkiller2009/ground/dispatcher/GroundDispatcherApplication.java
@@ -0,0 +1,13 @@
+package ru.deathkiller2009.ground.dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroundDispatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroundDispatcherApplication.class, args);
+    }
+
+}
